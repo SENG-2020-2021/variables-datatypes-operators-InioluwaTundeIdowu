5c204325e99c0bc125d7ce9de1a1c8a7aa1f0f93
InioluwaTundeId <tunde-idowu1020@student.babcock.edu.ng>
2020-09-25 23:34:20 +0000

    First Commit

--- Main.java
+++ Main.java
@@ -1,25 +1,64 @@
-class Main {
+//package com.company;
 
-  public static void main(String[] args) {
-   // kindly use appropriate data types for your declaration
+public class Main {
 
-   // declare variables days the debtor defaulted, amount the debtor is to pay per day,amount the debtor paid and assign values to them
-   int amount;
-   
-   //Declare the variable of the total amount the debtor is to pay, the days the debtor paid for, amount the debtor is left to pay, days the debtor has not payed for
+    public static void main(String[] args) {
+        // write your code here
 
-   // calculate and print total amount the debtor is to pay
+        // kindly use appropriate data types for your declaration
 
-   // calculate and print the days the debtor paid for
-  
-  // calculate and print the amount whose day was not captured because the amount was incomplete
+        // declare variables days the debtor defaulted, amount the debtor is to pay per day,amount the debtor paid and assign values to them
 
-   // calculate and print amount the debtor is left to pay
+        int defaultedDays = 10;
+        int amountPerDay = 1000;
+        int amountPaid = 3000;
 
-   // calculate and print days the debtor has not payed for
 
-   //kindly remove the statement below when you are done with the assignment
-    System.out.println("Hello world!");
-  }
+        //Declare the variable of the total amount the debtor is to pay, the days the debtor paid for, amount the debtor is left to pay, days the debtor has not payed for
 
+     
+        int numOfDaysPaid;
+        int amountToPay;
+        int daysToPay;
+        int amountLeftToPay;
+
+        // calculate and print total amount the debtor is to pay
+        amountToPay = defaultedDays * amountPerDay;
+        System.out.println("The amount to pay is " + amountToPay);
+
+
+        // calculate and print the days the debtor paid for
+        numOfDaysPaid = amountPaid / amountPerDay;
+        System.out.println("The number of days paid for is " + numOfDaysPaid + " day(s)");
+
+        // calculate and print the amount whose day was not captured because the amount was incomplete
+        int incompleteAmount=amountPaid%amountPerDay;
+        System.out.println("The incomplete amount not captured in a day is " + incompleteAmount);
+
+       
+		// calculate and print days the debtor has not paid for
+        daysToPay=defaultedDays-numOfDaysPaid;
+        System.out.println("The days Debtor has not paid for is " + daysToPay + " day(s)");
+		
+		 // calculate and print amount the debtor is left to pay
+        amountLeftToPay=amountToPay-amountPaid;
+        System.out.println("The amount left to pay is " + amountLeftToPay);
+
+      
+    }
 }
+
+
+               
+
+               
+
+               
+
+                
+
+
+
+
+
+
